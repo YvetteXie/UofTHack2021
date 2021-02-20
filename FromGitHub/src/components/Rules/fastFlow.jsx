30945a9e8f926ed9ac7f123a5ed2f201e696a046
Haoze Huang <oliver.h0803@gmail.com>
2021-02-19 23:22:34 -0500

    Delete FromGitHub directory

--- FromGitHub/src/components/Rules/fastFlow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default React.createClass({
-    render() {
-        return <div>
-            Press <big>[SPACEBAR]</big> to speed up the flow and score extra points!
-        </div>
-    }
-})
